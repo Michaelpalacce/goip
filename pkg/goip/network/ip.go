852a68ea0b23bd8e0fc11fb8fa4bcbf0983f4dc5
Stefan Genov <stefantigro@gmail.com>
2023-11-17 22:56:26 +0200

    feat: Using multiple providers

--- pkg/goip/network/ip.go
+++ pkg/goip/network/ip.go
@@ -1,7 +1,25 @@
 package network
 
+import (
+	"fmt"
+	"log/slog"
+)
+
 // GetPublicIp will fetch the public IP of the
 // machine that is running goip
 func GetPublicIp() ([]byte, error) {
-	return GetBody("https://icanhazip.com")
+	ipProviders := []string{"https://ifconfig.me/ip", "https://icanhazip.com"}
+
+	for _, provider := range ipProviders {
+		ip, err := GetBody(provider)
+
+		if err != nil {
+			slog.Error("Error while trying to fetch ip from provider", "error", err, "provider", provider)
+			continue
+		}
+
+		return ip, nil
+	}
+
+	return nil, fmt.Errorf("could not retrieve a response from any of the providers")
 }
